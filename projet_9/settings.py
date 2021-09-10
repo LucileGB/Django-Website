8a2bf392d391e969c0b02d87c67b74e1cf88f2a3
LucileGB <72474020+LucileGB@users.noreply.github.com>
2021-09-10 17:26:48 +0200

    Add files via upload

--- projet_9/settings.py
+++ projet_9/settings.py
@@ -12,12 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-vk+10nvc!ao)f$36+!n1kv6+mjs12^8ym6luh!@c-()8t4$1(%'
@@ -51,6 +48,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'projet_9.urls'
+
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = 'accueil'
+
+LOGOUT_REDIRECT_URL = 'login'
 
 TEMPLATES = [
     {
