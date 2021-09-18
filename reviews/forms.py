cb47719bd0bf74e1d35eaa970e33b4bb1534995d
Lucile Garrigoux <lgarrigoux@hotmail.com>
2021-09-18 09:48:41 +0200

    Working version so far

---
+++ reviews/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+from .models import Review, Ticket
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
+class AnswerTicket(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('headline', 'rating', 'body')
+
+class NewTicket(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ('title', 'description')
